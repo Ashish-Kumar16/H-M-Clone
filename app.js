ab6cdc2ae2ad586870aa37bc04630b3fcb1cc98e
Ashish-0016 <ashish.as6021@gmail.com>
2025-02-01 20:07:15 +0530

    update

--- app.js
+++ app.js
@@ -3,9 +3,9 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 const cookieParser = require("cookie-parser");
-const connectDB = require("./db");
+const connectDB = require("./server");
 const productRouter = require("./Routes/productRoute");
-const userRouter = require("../Backend/Routes/authRoute");
+const userRouter = require("./Routes/authRoute");
 const orderRouter = require("./Routes/orderRoute");
 const cartRouter = require("./Routes/cartRoute");
 const wishlistRouter = require("./Routes/wishRoute");
@@ -55,11 +55,4 @@ app.use((err, req, res, next) => {
 });
 
 // Start Server
-mongoose
-  .connect("mongodb://localhost:27017/HM-database")
-  .then(() => console.log("DB connected"))
-  .catch((err) => console.log(err));
-
-app.listen(PORT, () => {
-  console.log(`Server is running on ${PORT}`);
-});
+module.exports = app;
